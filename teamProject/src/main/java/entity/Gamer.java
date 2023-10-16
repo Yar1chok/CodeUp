d87a6ad2b8c100278ddb292ae9d875051e8635a1
Danil <danil_ustinov@mail.ru>
2023-10-16 15:31:53 +0700

    Добавлена бекэнд-часть авторизации и регистрации

--- teamProject/src/main/java/entity/Gamer.java
+++ teamProject/src/main/java/entity/Gamer.java
@@ -17,4 +17,38 @@ public class Gamer {
     public String password;
     @Column(name = "cur_lvl_java")
     public Integer curLvlJava;
+
+    public Gamer() {
+
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Integer getCurLvlJava() {
+        return curLvlJava;
+    }
+
+    public void setCurLvlJava(Integer curLvlJava) {
+        this.curLvlJava = curLvlJava;
+    }
+
+    public Gamer(String login, String password) {
+        this.login = login;
+        this.password = password;
+        this.curLvlJava = 1;
+    }
 }
