1af4382bd43cb499f52105ad73fe295dc76b3659
Igor Kovalev <Ig0rKovalev@mail.ru>
2023-11-29 14:13:51 +0700

    Страничка профиля и меню

---
+++ src/main/resources/static/js/menu.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+    $(".avatar").on("click", function(event){
+        event.stopPropagation();
+        $(".menu").toggle();
+    });
+
+    $(document).on("click", function(event){
+        if (!$(event.target).closest(".avatar_menu").length) {
+            $(".menu").hide();
+        }
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const buttons = document.querySelectorAll('.chapter');
+    const chapterName = document.querySelector('.chapter_name');
+    const description = document.querySelector('.description');
+
+    buttons.forEach(button => {
+        button.addEventListener('click', function() {
+            chapterName.textContent = this.textContent;
+            fetch('/texts/description.json')
+                .then(response => response.json())
+                .then(data => {
+                    const chapters = data.chapters;
+                    const selectedChapter = chapters.find(chapter => chapter.name === this.textContent);
+
+                    if (selectedChapter) {
+                        description.textContent = selectedChapter.description;
+                    } else {
+                        description.textContent = 'Описание отсутствует';
+                    }
+                })
+                .catch(error => console.error('Ошибка загрузки текстов:', error));
+        });
+    });
+});
